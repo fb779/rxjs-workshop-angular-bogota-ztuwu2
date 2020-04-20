ca6cf4443f6a476f4904b7cff0408ff29dc172ef
fb779 <fabianforero779@gmail.com>
2020-04-19 22:59:56 -0400

    3 files modified

--- src/app/giphy/giphy.component.ts
+++ src/app/giphy/giphy.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Observable } from 'rxjs';
 import { GiphyService } from "./giphy.service";
 import {
   API_SEARCH_URL,
@@ -14,13 +15,26 @@ import { Gif, GiphyResponse } from "./giphy.model";
   styleUrls: ["./giphy.component.css"]
 })
 export class GiphyComponent {
-  gifs: Gif[];
-  searchTerm: string = DEFAULT_SEARCH_TERM;
-  limit: number = DEFAULT_SEARCH_LIMIT;
+  // searchTerm: string = DEFAULT_SEARCH_TERM;
+  //limit: number = DEFAULT_SEARCH_LIMIT;
+  searchTerm$: Observable<string> = this.giphyService.searchTerm$;
+  limit$: Observable<number> = this.giphyService.limit$;
+  gifs$: Observable<Gif[]> = this.giphyService.gifs$;
+  totalResults$: Observable<Number> = this.giphyService.totalResults$;
+  totalPages$: Observable<Number> = this.giphyService.totalPages$;
+  actualPage$: Observable<Number> = this.giphyService.actualPage$;
 
-  constructor(private giphyService: GiphyService) {
-    this.giphyService.giphyResponse$.subscribe(
-      (response: GiphyResponse) => (this.gifs = response.data)
-    );
+  constructor(private giphyService: GiphyService) {}
+
+  movePage( num: number){
+    this.giphyService.movePage(num);
+  }
+
+  limitChange(num: number){
+    this.giphyService.limitChange(num);    
+  }
+
+  searchChange(term: string){
+    this.giphyService.searchTerm(term);
   }
 }
--- src/app/giphy/giphy.service.ts
+++ src/app/giphy/giphy.service.ts
@@ -15,7 +15,7 @@ import {
   catchError
 } from "rxjs/operators";
 import { GiphyResponse } from "./giphy.model";
-import { Observable, BehaviorSubject, combineLatest } from "rxjs";
+import { Observable, BehaviorSubject, combineLatest, of } from "rxjs";
 
 const giphyUrl = `${API_SEARCH_URL}?q=${DEFAULT_SEARCH_TERM}&api_key=${API_KEY}&limit=${DEFAULT_SEARCH_LIMIT}&offset=${DEFAULT_SEARCH_OFFSET}`;
 
@@ -23,6 +23,71 @@ const giphyUrl = `${API_SEARCH_URL}?q=${DEFAULT_SEARCH_TERM}&api_key=${API_KEY}&
   providedIn: "root"
 })
 export class GiphyService {
-  giphyResponse$ = this.http.get(giphyUrl);
+  private actualPageBS = new BehaviorSubject(0);
+  private searchTermBS = new BehaviorSubject(DEFAULT_SEARCH_TERM);
+  private limitBS = new BehaviorSubject(DEFAULT_SEARCH_LIMIT);
+  
+  actualPage$ = this.actualPageBS.asObservable();
+  searchTerm$ = this.searchTermBS.asObservable();
+  limit$ = this.limitBS.asObservable();
+
+  // giphyResponse$ = this.http.get(giphyUrl);
+
+  /*giphyResponse$ = this.actualPage$.pipe(
+    switchMap( 
+      actualPage => this.http.get( this.getUrl({offset: actualPage} ) )
+    )
+  )*/
+
+  giphyResponse$ = combineLatest([
+    this.searchTerm$,
+    this.actualPage$, 
+    this.limit$
+  ]).pipe(
+    switchMap( 
+      ([term, actualPage, limit]) => 
+      this.http.get( this.getUrl({
+        term, actualPage, limit
+      } ) )
+    )
+  )
+
+  gifs$ = this.giphyResponse$.pipe(
+    map( (response: GiphyResponse) => response.data )
+  );
+
+  totalResults$ = this.giphyResponse$.pipe(
+    map( (response: GiphyResponse) => response.pagination.total_count )
+  );
+
+  totalPages$ = this.totalResults$.pipe(
+    withLatestFrom( this.limit$),
+    map( ( [totalResoults, limit] ) => Math.ceil(totalResoults / limit))
+  )
+
   constructor(private http: HttpClient) {}
+
+  private getUrl( params: any ){
+    let term = params.term || DEFAULT_SEARCH_TERM, 
+        limit = params.limit || DEFAULT_SEARCH_LIMIT, 
+        actualPage = params.actualPage || DEFAULT_SEARCH_OFFSET;
+
+    return `${API_SEARCH_URL}?q=${term}&api_key=${API_KEY}&limit=${limit}&offset=${actualPage * DEFAULT_SEARCH_LIMIT}`;
+  }
+
+  movePage(num: number){
+    this.actualPageBS.next(
+      this.actualPageBS.getValue()+num
+    ) 
+  }
+
+  searchTerm( term:string ){
+    this.searchTermBS.next( term );
+    this.actualPageBS.next(0)
+  }
+  
+  limitChange( limit:number ){
+    this.limitBS.next(limit);
+    this.actualPageBS.next(0)
+  }
 }
