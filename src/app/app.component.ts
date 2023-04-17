f9b92ccfd23290d7f97141f50309b2d622490d22
fb779 <fb779@users.noreply.github.com>
2023-04-16 21:55:51 -0400

    Logo Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-app",
+  selector: 'my-app',
   template: `
     <mat-toolbar color="primary">
       <img [src]="logo" class="logo" /> {{ name }}
@@ -15,10 +15,12 @@ import { Component } from "@angular/core";
         height: 60px;
         width: auto;
       }
-    `
-  ]
+    `,
+  ],
 })
 export class AppComponent {
-  name = "Workshop RxJS";
-  logo = "https://pappcornbackapp.com/logos/logo_angular_bogota.png";
+  name = 'Workshop RxJS';
+  logo =
+    'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg';
+  // logo = "https://pappcornbackapp.com/logos/logo_angular_bogota.png";
 }
